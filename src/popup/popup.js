543936917f2ed818c0b447e5818648ca4f1459c8
Anthony Jiang <anthony.zyjiang@outlook.com>
2022-01-04 12:24:29 +0000

    added update notification

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,28 @@
 'use strict';
 
+const MANIFEST_URL = 'https://raw.githubusercontent.com/tmxkn1/Microsoft-Reward-Chrome-Ext/master/src/manifest.json';
+
 document.addEventListener('DOMContentLoaded', () => {
     chrome.runtime.sendMessage({
         action: 'checkStatus',
     });
 });
+
+function checkUpdate() {
+    fetch(MANIFEST_URL, {method: 'GET'}).then((response) => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Fetch failed.');
+    }).then((manifest) => {
+        const currentVersion = chrome.runtime.getManifest().version;
+        const latestVersion = manifest.version;
+        if (currentVersion !== latestVersion) {
+            document.getElementById('update-available').style.display = 'block';
+        } else {
+            document.getElementById('update-available').style.display = 'none';
+        }
+    });
+}
+
+checkUpdate();
