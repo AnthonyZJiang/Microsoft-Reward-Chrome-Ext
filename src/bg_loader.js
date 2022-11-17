f1f15a09c06706cdcc74f0629eaa470c13e7a0a4
Zmey1 <52914722+Zmey1@users.noreply.github.com>
2022-11-17 16:20:23 +0530

    Create bg_loader.js
    This is used to import all the js scripts since service worker can only import one

---
+++ src/bg_loader.js
@@ -0,0 +1,15 @@
+try {
+    importScripts(
+    "status/DailyQuest.js",
+    "status/DailyRewardStatus.js",
+    "quest/searchQuest.js",
+    "quest/quizDailyQuest.js",
+    "googleTrend.js",
+    "utility.js",
+    "badge.js",
+    "exception.js",
+    "background.js",
+    "test.js");
+  } catch (e) {
+        console.log(e);
+  }
