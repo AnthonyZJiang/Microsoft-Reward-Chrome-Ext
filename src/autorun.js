775af5350cc29a883edb840137f47af56aaeec67
Your Name <you@example.com>
2023-03-02 18:12:47 +0100

    contentscript. Not working. cant add a delay

--- src/autorun.js
+++ src/autorun.js
@@ -1,1 +1,20 @@
-console.log("sussy")
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
+ }
+
+function runScript(){
+    var s = document.createElement('script');
+s.src = chrome.runtime.getURL('solve.js');
+s.onload = function() {
+    this.remove();
+};
+document.body.appendChild(s);
+}
+
+
+
+runScript();
